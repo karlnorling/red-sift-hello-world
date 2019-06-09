6654afa5d657e28ca94d0662b56a33108afe427e
Karl Norling <karl.whatever@gmail.com>
2019-06-09 12:40:01 +0100

    more

--- frontend/src/scripts/controller.js
+++ frontend/src/scripts/controller.js
@@ -7,14 +7,14 @@ export default class MyController extends SiftController {
   constructor() {
     // You have to call the super() method to initialize the base class.
     super();
-    // this._suHandler = this.onStorageUpdate.bind(this);
+    this._suHandler = this.onStorageUpdate.bind(this);
   }
 
   // for more info: http://docs.redsift.com/docs/client-code-siftcontroller
   loadView(state) {
     console.log('hello-world: loadView', state);
     // Register for storage update events on the "x" bucket so we can update the UI
-    // this.storage.subscribe(['x'], this._suHandler);
+    this.storage.subscribe(['who'], this._suHandler);
     switch (state.type) {
       case 'email-thread':
         return {
@@ -24,7 +24,10 @@ export default class MyController extends SiftController {
       case 'summary':
         return {
           html: 'summary.html',
-          data: {} //this.getX()
+          data: this.getWebhook().then(d => ({
+            name: 'no-one',
+            webhook_uri: d
+          }))
         };
       default:
         console.error('hello-world: unknown Sift type: ', state.type);
@@ -32,22 +35,31 @@ export default class MyController extends SiftController {
   }
 
   // Event: storage update
-  // onStorageUpdate(value) {
-  //   console.log('hello-world: onStorageUpdate: ', value);
-  //   return this.getX().then(xe => {
-  //     // Publish events from 'x' to view
-  //     this.publish('counts', xe);
-  //   });
-  // }
+  onStorageUpdate(value) {
+    console.log('hello-world: onStorageUpdate: ', value);
+    return this.getWho().then(xe => {
+      // Publish events from 'x' to view
+      this.publish('name', xe);
+    });
+  }
 
-  //  getX() {
-  //   return this.storage.getAll({
-  //     bucket: 'x'
-  //   }).then((values) => {
-  //     console.log('hello-world: getX returned:', values);
-  //     return {};
-  //   });
-  // }
+  getWebhook() {
+    return this.storage.get({
+      bucket: '_redsift',
+      keys: ['webhooks/curl_input'],
+    }).then((d) => d[0].value);
+  }
+
+  getWho() {
+    return this.storage.getAll({
+      bucket: 'who'
+    }).then((values) => {
+      console.log('hello-world: getWho returned:', values);
+      return {
+        name: values[0].value
+      };
+    });
+  }
 
 }
 
--- frontend/src/scripts/view.js
+++ frontend/src/scripts/view.js
@@ -3,22 +3,36 @@
  */
 import { SiftView, registerSiftView } from '@redsift/sift-sdk-web';
 
+const dataKey = 'name';
+
 export default class MyView extends SiftView {
   constructor() {
     // You have to call the super() method to initialize the base class.
     super();
+    this.controller.subscribe(dataKey, this.onHello.bind(this));
 
   }
 
   // for more info: http://docs.redsift.com/docs/client-code-siftview
   presentView(value) {
     console.log('hello-world: presentView: ', value);
+    document.getElementById(dataKey).textContent = value.data[dataKey];
+    const webhookUri = value.data['webhook_uri'];
+    const webhookUriElement = document.getElementById('webhook_uri');
+    webhookUriElement.getElementsByTagName('code')[0].textContent = `curl ${webhookUri}`;
+    webhookUriElement.setAttribute('href', webhookUri);
   };
 
   willPresentView(value) {
     console.log('hello-world: willPresentView: ', value);
   };
 
+  onHello(data) {
+    console.log('tutorial-sift: onHello:', data);
+    Object.keys(data).forEach((k) => {
+      document.getElementById(k).textContent = data[k];
+    });
+  }
 }
 
 registerSiftView(new MyView(window));
